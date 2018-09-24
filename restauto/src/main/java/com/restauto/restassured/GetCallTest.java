14da5e5cd9132e35235160186a096456c97a1bc6
dasm <dasm@LUW7MDAS3.Informatics.qiagen.ads>
2018-09-23 18:00:50 -0700

    add getCall

--- restauto/src/main/java/com/restauto/restassured/GetCallTest.java
+++ restauto/src/main/java/com/restauto/restassured/GetCallTest.java
@@ -1,9 +1,23 @@
 package com.restauto.restassured;
 
+import static io.restassured.RestAssured.given;
+
+import io.restassured.response.Response;
+
 public class GetCallTest {
 
 	public GetCallTest() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	 public void getCall() {
+		  System.out.println("........... 1.  ..........");
+			Response res=   given().
+					get("http://services.groupkt.com/state/get/IND/all");
+			System.out.println("response = " + res.asString());
+			
+			System.out.println(" ............... 2    ......");
+			
+	 }
 
 }
